bd9f3b0a21db04ea8f9e1c73b95f183fcac3ef58
Cameron Smith <theauspiciouscammy@gmail.com>
2021-06-28 11:12:59 -0700

    6/28 - table rendering complete

--- htmlTables/server.py
+++ htmlTables/server.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    usersInfo = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+    return render_template('index.html', users = usersInfo)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
