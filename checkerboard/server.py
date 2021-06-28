cd0bd9b725f65773e39172459c21f446e718ef4b
Cameron Smith <theauspiciouscammy@gmail.com>
2021-06-28 10:15:28 -0700

    6/28 - Checkerboard complete

--- checkerboard/server.py
+++ checkerboard/server.py
@@ -3,7 +3,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', row=int(8), col=int(8))
+
+@app.route('/<x>')
+def resizeRow(x):
+    return render_template('index.html', row=int(x),col=int(8))
+
+@app.route('/<x>/<y>')
+def resizeRowCol(x, y):
+    return render_template('index.html', row=int(x),col=int(y))
+
+@app.route('/<x>/<y>/<c1>/<c2>')
+def resize_with_color(x, y, c1, c2):
+    return render_template('index.html', row=int(x), col=int(y), color1=c1, color2=c2)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
