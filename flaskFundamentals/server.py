6884e92db6cad42dbcaa05bd9c956bdbe444849c
Cameron Smith <theauspiciouscammy@gmail.com>
2021-06-29 07:29:23 -0700

    6/29 - NOTES

--- flaskFundamentals/server.py
+++ flaskFundamentals/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = 'Dont take any wooden nickles.'
 
 @app.route('/')
 def index():
@@ -18,6 +19,28 @@ def renderLists():
         {'name' : 'KB', 'age' : 27}
     ]
     return render_template("lists.html", randomNumbers = [3,1,5], students = studentInfo)
+
+@app.route('/form')
+def formGet():
+    return render_template('form.html')
+
+@app.route('/form', methods=['POST'])
+def create_user():
+    print("Got Post Info")
+    print(request.form)
+    if request.form['action'] == 'register':
+        session['username'] = request.form['name']
+        session['useremail'] = request.form['email']
+    elif request.form['action'] == 'login':
+        session['loginEmail'] = request.form['email']
+        session['loginPass'] = request.form['password']
+    return redirect("/show")
+
+@app.route('/show')
+def showUser():
+    print("Showing the User Info From the Form")
+    print(request.form)
+    return render_template('show.html')
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
