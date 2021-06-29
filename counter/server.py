b6d7e68ccd08b9e251af00e540dc325fd66af5fd
Cameron Smith <theauspiciouscammy@gmail.com>
2021-06-29 07:27:33 -0700

    assignment complete

---
+++ counter/server.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'secret_keyy'
+
+@app.route('/')
+def index():
+    if 'count' not in session:
+        session['count'] = 1
+        print(session['count'])
+    else:
+        session['count'] += 1
+        print(session['count'])
+    return render_template('index.html')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect("/")
+
+@app.route('/twice')
+def doubleDown():
+    session['count'] += 2
+    print(session['count'])
+    return render_template('index.html')
+
+# @app.route('/form', methods=['POST'])
+# def buttons():
+#     if request.form['action'] == 'reset':
+#         session.clear()
+#         return redirect("/")
+#     elif request.form['action'] == 'twice':
+#         session['count'] += 2
+#         print(session['count'])
+#         return render_template('index.html')
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return 'Invalid URL.'
+if __name__=="__main__":
+    app.run(debug=True)
