e199a2d2311539f0b44c152c3b72362de9ec6c63
Cameron Smith <theauspiciouscammy@gmail.com>
2021-07-03 10:18:29 -0700

    6/3

--- numberGame/server.py
+++ numberGame/server.py
@@ -1,14 +1,50 @@
 from flask import Flask, render_template, request, redirect, session
 import random
+
 app = Flask(__name__)
 app.secret_key = 'secret_keyy'
 
-winningNum = random.randint(1, 100)
-print(winningNum)
-
 @app.route('/')
 def index():
+    # winningNum = random.randint(1, 100)
+    # print(winningNum)
+    if 'turns' not in session:
+        session['turns'] = 0
+        session['correct_guesses'] = 0
+        session['wrong_guesses'] = 0
+        session['prev_guesses'] = []
+        session['num'] = random.randint(1, 100)
+    # session['num'] = winningNum
     return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def processForm():
+    #This Method always returns false
+    session['prev_guesses'].append(request.form['guess'])
+    session['turns']+=1
+    print(request.form['guess'])
+    print(session['num'])
+    if request.form['guess'] == session['num']:
+        session['result'] = True
+        session['correct_guesses']+=1
+        print(session['result'])
+        # return redirect("/results")
+    else:
+        session['result'] = False
+        session['wrong_guesses']+=1
+        print(session['result'])
+    return redirect("/results")
+
+
+@app.route('/results')
+def results():
+    win_percentage = session['correct_guesses']// session['turns']
+    return render_template('results.html', percent = win_percentage)
+
+@app.route('/reset')
+def resetGame():
+    session.clear()
+    return redirect("/")
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
