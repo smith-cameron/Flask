47345b1045e362616d7e01c41320035e0a24c5cd
Cameron Smith <theauspiciouscammy@gmail.com>
2021-07-10 11:15:32 -0700

    initialized users project

---
+++ users/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, session
+from mysqlconnection import connectToMySQL
+
+app = Flask(__name__)
+app.secret_key = 'secret_keyy'
+mysql = connectToMySQL('initflask')
+
+@app.route('/')
+def index():
+    users = mysql.query_db('SELECT * FROM users;')
+    print(users)
+    return render_template('index.html')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return 'Invalid URL.'
+if __name__=="__main__":
+    app.run(debug=True)
