6fd01e84fc6a5ae476beccb4e4f45a5f56dfbf75
Cameron Smith <theauspiciouscammy@gmail.com>
2021-07-10 08:27:38 -0700

    updated skeleton w/mysql connection

--- ninjaGold/server.py
+++ ninjaGold/server.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template, request, redirect, session
-from mysqlconnection import connectToMySQL
 
 app = Flask(__name__)
 app.secret_key = 'secret_keyy'
-mysql = connectToMySQL('initflask')
-users = mysql.query_db('SELECT * FROM users;')
 
 @app.route('/')
 def index():
-    print(users)
     return render_template('index.html')
 
 @app.route('/', defaults={'path': ''})
