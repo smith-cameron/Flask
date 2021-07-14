98f7fc7e2102ec22b79e2160c967108a320722ab
Cameron Smith <theauspiciouscammy@gmail.com>
2021-07-13 20:27:12 -0700

    project initialized, DB tables complete

---
+++ basicValidations/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, session
+from mysqlDB import connectToMySQL
+from datetime import datetime
+
+app = Flask(__name__)
+app.secret_key = 'secret_keyy'
+dateFormat = "%m/%d/%Y"
+
+@app.route('/')
+def index():
+    mysql = connectToMySQL('initflask')
+    users = mysql.query_db('SELECT * FROM users;')
+    print(users)
+    return render_template('index.html')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return 'Invalid URL.'
+if __name__=="__main__":
+    app.run(debug=True)
