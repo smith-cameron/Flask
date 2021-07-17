9a96097e729080edb5816a99ec745cbce6a98bdc
Cameron Smith <theauspiciouscammy@gmail.com>
2021-07-17 14:07:01 -0700

    iterim commit

--- basicValidations/server.py
+++ basicValidations/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash
 from mysqlDB import connectToMySQL
 from datetime import datetime
 
@@ -8,10 +8,52 @@ dateFormat = "%m/%d/%Y"
 
 @app.route('/')
 def index():
-    mysql = connectToMySQL('initflask')
-    users = mysql.query_db('SELECT * FROM users;')
-    print(users)
-    return render_template('index.html')
+    mysql = connectToMySQL('basicValidations')
+    locs = mysql.query_db('SELECT * FROM locations;')
+    mysql = connectToMySQL('basicValidations')
+    langs = mysql.query_db('SELECT * FROM languages;')
+    mysql = connectToMySQL('basicValidations')
+    allNinjas = mysql.query_db('SELECT * FROM ninjas;')
+    # print(locs)
+    # print(langs)
+    return render_template('index.html', locations = locs, languages = langs, ninjas = allNinjas)
+
+@app.route('/postData', methods=['POST'])
+def create():
+    if len(request.form['userName']) < 1:
+        flash("Name Required")
+    if request.form['location'] == 0:
+        flash("Location Required")
+    if request.form['language'] == 0:
+        flash("Language Required")
+    if request.form['comment']:
+        if len(request.form['comment']) < 5 and len(request.form['comment']) > 0:
+            flash("Comment Must Be Over 10 Characters")
+    if not '_flashes' in session.keys():
+        # print(request.form)
+        mysql = connectToMySQL('basicValidations')
+        query = "INSERT INTO ninjas (UserName, locId, langId, comment, createdAt) VALUES (%(n)s, %(lo)s, %(la)s, %(com)s, NOW());"
+        input = {
+            "n" : request.form["userName"],
+            "lo" : request.form["location"],
+            "la" : request.form["language"],
+            "com" : request.form["comment"],
+        }
+        ninja = mysql.query_db(query, input)
+        return redirect(f'/success/{ninja}')
+    return redirect('/')
+
+@app.route('/success/<id>')
+def resultsRender(id):
+    mysql = connectToMySQL('basicValidations')
+    query = ('SELECT * FROM ninjas WHERE id = %(n)s;')
+    input = {
+        "n" : id
+    }
+    thisNinja = mysql.query_db(query, input)
+    print(thisNinja)
+
+    return render_template('result.html', ninja = thisNinja)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
