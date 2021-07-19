a465760a48ad8b3f4db51c6b99a4863b99fa8ad1
Cameron Smith <theauspiciouscammy@gmail.com>
2021-07-18 20:23:28 -0700

    dojoSurvey cleanup

--- dojoSurvey/server.py
+++ dojoSurvey/server.py
@@ -7,14 +7,29 @@ app.secret_key = 'secret_keyy'
 def index():
     return render_template('index.html')
 
-@app.route('/result', methods=['POST'])
-def results():
+# @app.route('/post', methods=['POST'])
+# def postData():
+#     print(request.form)
+#     return render_template('result.html',
+#     name = request.form['userName'],
+#     location = request.form['location'],
+#     language = request.form['language'],
+#     comment = request.form['comment'])
+
+@app.route('/post', methods=['POST'])
+def postData():
     print(request.form)
+    session['userName'] = request.form['userName']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comment'] = request.form['comment']
+    return redirect('/result')
+
+@app.route('/result')
+def results():
     return render_template('result.html',
-    name = request.form['userName'],
-    location = request.form['location'],
-    language = request.form['language'],
-    comment = request.form['comment'])
+    language = session['language'],
+    comment = session['comment'])
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
