7b91346f727f15049a4ab38c07d16753f5d8a86e
theauspiciouscammy <theauspiciouscammy@gmail.com>
2022-02-16 00:19:39 -0800

    update

--- wall/server.py
+++ wall/server.py
@@ -3,9 +3,9 @@ from wallApp.controllers import main, loginReg, posts
 
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
+@app.route('/', defaults={'cookies': ''})
+@app.route('/<path:cookies>')
+def catch_all(cookies):
     return 'Invalid URL.'
 if __name__=="__main__":
     app.run(debug=True)
