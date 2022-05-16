8b0ce70b79ca75934e36c533f5fbad7337114be0
theauspiciouscammy <theauspiciouscammy@gmail.com>
2022-05-16 09:26:21 -0700

    update

--- books/flask_app/models/author.py
+++ books/flask_app/models/author.py
@@ -1,4 +1,5 @@
 from flask_app.config.mysqlDB import connect
+myDB = "flaskbooks"
 
 class Author:
     def __init__(self, data):
@@ -10,32 +11,32 @@ class Author:
     @classmethod
     def getAll(cls):
         query = 'SELECT * FROM authors;'
-        allAuth = connect('booksAuthors').query_db(query)
+        allAuth = connect(myDB).query_db(query)
         return allAuth
 
     @classmethod
     def save(cls, data):
         query = 'Insert INTO authors (name, createdAt) VALUES( %(n)s, NOW());'
-        authId = connect('booksAuthors').query_db(query, data)
+        authId = connect(myDB).query_db(query, data)
         return authId
 
     @classmethod
     def findById(cls, data):
         query = 'SELECT * FROM authors WHERE id = %(i)s;'
-        thisAuth = connect('booksAuthors').query_db(query, data)
+        thisAuth = connect(myDB).query_db(query, data)
         return thisAuth
 
     @classmethod
     def saveFavs(cls, data):
         query = 'Insert INTO favorites (book_id, author_id) VALUES( %(bi)s, %(ai)s);'
-        newFav = connect('booksAuthors').query_db(query, data)
+        newFav = connect(myDB).query_db(query, data)
         return newFav
 
     @classmethod
     def getFav(cls, data):
         # Need to update query to return whole opjects
         query = 'SELECT b.title FROM favorites f JOIN books b ON book_id = b.id WHERE author_id = %(i)s GROUP BY title;'
-        authorFavsFromDB = connect('booksAuthors').query_db(query, data)
+        authorFavsFromDB = connect(myDB).query_db(query, data)
         favs = []
         for i in authorFavsFromDB:
             for val in i:
--- books/flask_app/models/book.py
+++ books/flask_app/models/book.py
@@ -1,5 +1,6 @@
 from flask_app.config.mysqlDB import connect
 from flask_app.models.author import Author
+myDB = "flaskbooks"
 
 class Book:
     def __init__(self, data):
@@ -12,35 +13,35 @@ class Book:
     @classmethod
     def getAll(cls):
         query = 'SELECT * FROM books;'
-        allBooks = connect('booksAuthors').query_db(query)
+        allBooks = connect(myDB).query_db(query)
         return allBooks
 
     @classmethod
     def save(cls, data):
         query = 'Insert INTO books (title, pageCount, createdAt) VALUES( %(t)s, %(pc)s, NOW());'
-        bookId = connect('booksAuthors').query_db(query, data)
+        bookId = connect(myDB).query_db(query, data)
         return bookId
 
     @classmethod
     def findById(cls, data):
         query = 'SELECT * FROM books WHERE id = %(i)s;'
-        thisBook = connect('booksAuthors').query_db(query, data)
+        thisBook = connect(myDB).query_db(query, data)
         return thisBook
 
     @classmethod
     def saveFavs(cls, data):
         query = 'Insert INTO favorites (book_id, author_id) VALUES( %(bi)s, %(ai)s);'
-        newFav = connect('booksAuthors').query_db(query, data)
+        newFav = connect(myDB).query_db(query, data)
         return newFav
 
     @classmethod
     def getFav(cls, data):
         query = 'SELECT * FROM favorites f JOIN authors a ON author_id = a.id WHERE book_id = %(i)s;'
-        bookFavsFromDB = connect('booksAuthors').query_db(query, data)
+        bookFavsFromDB = connect(myDB).query_db(query, data)
         return bookFavsFromDB
 
     @classmethod
     def getNotFav(cls, data):
         query = 'SELECT a.name FROM favorites f JOIN authors a ON author_id = a.id WHERE book_id != %(i)s GROUP BY a.name;'
-        notFavs = connect('booksAuthors').query_db(query, data)
+        notFavs = connect(myDB).query_db(query, data)
         return notFavs
